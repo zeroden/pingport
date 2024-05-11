d8b33692690cd2e6b309cc5a9f77d2e462897115
zeroden <9961482+zeroden@users.noreply.github.com>
2024-05-11 14:28:56 +0600

    add manual ping add day stat

--- pingport.py
+++ pingport.py
@@ -32,13 +32,21 @@ origstdout = sys.stdout
 DupeConsoleToFile(timestamp)
 print(sys.version)
 print(sys.executable)
+print("Press F1 for a manual ping")
 timestamp = time.strftime('[%Y-%m-%d %H:%M:%S pingport started]')
 print(timestamp)
-print("press f1 for next ping")
+
+ping_series_ok = 0
+ping_fails = 0
+ping_fails_str = ''
 
 def sleep(i):
 	while i:
-		win32api.SetConsoleTitle('pingport %d' %i)
+		if ping_fails:
+			ping_fails_str = ' (fails %d)' % ping_fails
+			win32api.SetConsoleTitle('pingport %d%s' % (i, ping_fails_str))
+		else:
+			win32api.SetConsoleTitle('pingport %d' % i)
 		i = i - 1
 		j = 10
 		while j:
@@ -50,51 +58,82 @@ def sleep(i):
 				j = 0
 
 def percentage(whole, part):
-	perc = 100 * float(part) / float(whole)
+	if whole:
+		perc = 100 * float(part) / float(whole)
+	else:
+		perc = 0
 	return str(round(perc))
 
 sock = None
-timemark_prev = time.time()
+timemark_prev_hour = time.time()
+timemark_prev_day = time.time()
 ping_hour_attempts = 0
+ping_day_attempts = 0
 ping_hour_ok = 0
+ping_day_ok = 0
 while True:
 	timestamp = time.strftime('%H:%M:%S')
 	timemark_now = time.time()
-	timediff = (timemark_now - timemark_prev) / 3600
+
+	timediff = (timemark_now - timemark_prev_hour) / 3600
+	# print hour stat
 	if (timediff >= 1):
-		timemark_prev = timemark_now
+		# reset hour marker
+		timemark_prev_hour = timemark_now
 		perc = percentage(ping_hour_attempts, ping_hour_ok)
-		ping_hour_attempts = 0
-		ping_hour_ok = 0
 		if (timediff >= 2):
 			print('%s +%d hours' % (time.strftime('[%Y-%m-%d %H:%M:%S]'), timediff))
 		else:
-			print(time.strftime('[%Y-%m-%d %H:%M:%S]') + ' hour uptime %s%%' % perc)
+			if ping_hour_attempts == ping_hour_ok:
+				print(time.strftime('[%Y-%m-%d %H:%M:%S]') + ' hour uptime 100%' + ping_fails_str)
+			else:
+				print(time.strftime('[%Y-%m-%d %H:%M:%S]') + ' hour partial uptime %s%%' % perc + ping_fails_str)
+		# reset hour counters
+		ping_hour_attempts = 0
+		ping_hour_ok = 0
+
+	# print day stat
+	timediff = (timemark_now - timemark_prev_day) / 3600
+	if (timediff >= 24):
+		# reset day marker
+		timemark_prev_day = timemark_now
+		perc = percentage(ping_day_attempts, ping_day_ok)
+		if ping_day_attempts == ping_day_ok:
+			print(time.strftime('[%Y-%m-%d %H:%M:%S]') + ' day uptime 100%' + ping_fails_str)
+		else:
+			print(time.strftime('[%Y-%m-%d %H:%M:%S]') + ' day partial uptime %s%%' % perc + ping_fails_str)
+		# reset day counters
+		ping_day_attempts = 0
+		ping_day_ok = 0
 
 	if sock:
 		sock.close()
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	
 	ping_hour_attempts += 1
+	ping_day_attempts += 1
 	
-	# 87.250.250.242 ya.ru
 	# 5.255.255.242 ya.ru
 	result = sock.connect_ex(('5.255.255.242', 80))
 	if result == 0:
-		print('%s up' % timestamp)
 		ping_hour_ok += 1
+		ping_day_ok += 1
+		ping_series_ok += 1
+		print('%s up %d' % (timestamp, ping_series_ok))
 	else:
-		# 91.198.174.192 wikipedia.org
 		# 185.15.59.224 wikipedia.org
 		result = sock.connect_ex(('185.15.59.224', 80))
 		if result == 0:
-			print('%s up2' % timestamp)
 			ping_hour_ok += 1
-			sleep(5)
+			ping_series_ok += 1
+			print('%s up2 %d' % (timestamp, ping_series_ok))
+			sleep(10)
 			continue
 		else:
-			print('%s down' % timestamp)
-			sleep(5)
+			ping_fails += 1
+			ping_series_ok = 0
+			print('%s down %d' % (timestamp, ping_fails))
+			sleep(10)
 			continue
 
-	sleep(60) # 60 slow, 30 fast
+	sleep(60)
